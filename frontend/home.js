954cf1578a3c5d8d768baa2bd1b0aa4967f2ffea
lowmoya <lowmoya@gmail.com>
2024-04-06 22:15:54 -0700

    alarm

--- frontend/home.js
+++ frontend/home.js
@@ -1,6 +1,7 @@
 let nav = 0;
 let clicked = {};
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : {};
+let alarms = localStorage.getItem('alarms') || [];
 
 const calendar = document.getElementById('calendar');
 const newEventModal =  document.getElementById('newEventModal');
